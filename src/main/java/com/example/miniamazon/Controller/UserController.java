81fe2600b5dded29bce12b420619fc415dff01ae
MohamedYehia20m <77248801+MohamedYehia20m@users.noreply.github.com>
2024-12-29 17:54:46 +0200

    intializing controllers (UserController)

---
+++ src/main/java/com/example/miniamazon/Controller/UserController.java
@@ -0,0 +1,51 @@
+package com.example.miniamazon.Controller;
+
+
+import com.example.miniamazon.Model.User;
+import com.example.miniamazon.Service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;   //response entity is a class that represents the entire HTTP response: status code, headers, and body
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.createUser(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    /*
+    @GetMapping("/search")
+    public ResponseEntity<List<User>> searchUsers(@RequestParam String query) {
+        return ResponseEntity.ok(userService.searchUsers(query));
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<User>> getUsersByCategory(@PathVariable String category) {
+        return ResponseEntity.ok(userService.getUsersByCategory(category));
+    }
+    */
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id,  @RequestBody User user) {
+        return ResponseEntity.ok(userService.updateUser(id, user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.deleteUser(id)).getBody();
+    }
+
+
+}
