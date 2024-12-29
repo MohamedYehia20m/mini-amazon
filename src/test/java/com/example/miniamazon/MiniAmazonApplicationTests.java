285a06014ce6036d9eda7f029553755714f9d23d
MohamedYehia20m <77248801+MohamedYehia20m@users.noreply.github.com>
2024-12-29 17:59:48 +0200

    intializing SpringBootApplication tests

--- src/test/java/com/example/miniamazon/MiniAmazonApplicationTests.java
+++ src/test/java/com/example/miniamazon/MiniAmazonApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MiniAmazonApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
