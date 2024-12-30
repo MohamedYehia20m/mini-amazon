811f9bce45ba353cf6749dbd1675c885b64f0aae
MohamedYehia20m <77248801+MohamedYehia20m@users.noreply.github.com>
2024-12-30 13:31:31 +0200

    init Seller Controller

--- src/main/java/com/example/miniamazon/Controller/SellerController.java
+++ src/main/java/com/example/miniamazon/Controller/SellerController.java
@@ -1,4 +1,46 @@
 package com.example.miniamazon.Controller;
 
+
+import com.example.miniamazon.Model.Seller;
+import com.example.miniamazon.Service.SellerService;
+import lombok.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/sellers")
 public class SellerController {
+
+    private final SellerService sellerService;
+
+    @GetMapping
+    public ResponseEntity<List<Seller>> getAllSellers() {
+        return ResponseEntity.ok(sellerService.getAllSellers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Seller> getSeller(@PathVariable Long id) {
+        return ResponseEntity.ok(sellerService.getSellerById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Seller> addSeller(@RequestBody Seller seller) {
+        return ResponseEntity.ok(sellerService.addSeller(seller));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Seller> updateSeller(@PathVariable Long id, @RequestBody Seller seller) {
+        return ResponseEntity.ok(sellerService.updateSeller(id, seller));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSeller(@PathVariable Long id) {
+        sellerService.deleteSeller(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
