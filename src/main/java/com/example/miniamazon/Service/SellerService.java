527d1c1d16393faf0b394134a7a997ba7e39770d
MohamedYehia20m <77248801+MohamedYehia20m@users.noreply.github.com>
2024-12-30 13:32:17 +0200

    init Seller Service

--- src/main/java/com/example/miniamazon/Service/SellerService.java
+++ src/main/java/com/example/miniamazon/Service/SellerService.java
@@ -1,4 +1,49 @@
 package com.example.miniamazon.Service;
 
+import com.example.miniamazon.Repository.SellerRepository;
+import lombok.*;
+import org.springframework.stereotype.*;
+import com.example.miniamazon.Model.Seller;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class SellerService {
+
+    private final SellerRepository sellerRepository;
+
+    public List<Seller> getAllSellers() {
+        return sellerRepository.findAll();
+    }
+
+    public Seller getSellerById(Long id) {
+        return sellerRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Seller not found"));
+    }
+
+    public Seller addSeller(Seller seller) {
+        return sellerRepository.save(seller);
+    }
+
+    public Seller updateSeller(Long id, Seller sellerDetails) {
+        Seller seller = getSellerById(id);
+        seller.setSellerName(sellerDetails.getSellerName());
+        seller.setSellerEmail(sellerDetails.getSellerEmail());
+        seller.setSellerAddress(sellerDetails.getSellerAddress());
+        seller.setSellerPhone(sellerDetails.getSellerPhone());
+        seller.setSellerBankAccount(sellerDetails.getSellerBankAccount());
+        return sellerRepository.save(seller);
+    }
+
+    public void deleteSeller(Long id) {
+        sellerRepository.deleteById(id);
+    }
+
+    /**/
+
+
+
+
+
+
 }
