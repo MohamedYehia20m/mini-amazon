9386c7f1f313dc3cf559937a9ddc3f21933df950
MohamedYehia20m <77248801+MohamedYehia20m@users.noreply.github.com>
2024-12-30 11:48:52 +0200

    init Product Controller

--- src/main/java/com/example/miniamazon/Controller/ProductController.java
+++ src/main/java/com/example/miniamazon/Controller/ProductController.java
@@ -1,4 +1,49 @@
 package com.example.miniamazon.Controller;
 
+
+import com.example.miniamazon.Service.ProductService;
+import lombok.*;
+import com.example.miniamazon.Model.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+        return ResponseEntity.ok(productService.addProduct(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return ResponseEntity.ok(productService.updateProduct(id, product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    /*
+     */
+
+
 }
