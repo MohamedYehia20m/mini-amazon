d8072abb31a5cf7250d83788d4ee38b3e7cf2681
MohamedYehia20m <77248801+MohamedYehia20m@users.noreply.github.com>
2024-12-30 13:33:09 +0200

    init Seller Repository

--- src/main/java/com/example/miniamazon/Repository/SellerRepository.java
+++ src/main/java/com/example/miniamazon/Repository/SellerRepository.java
@@ -1,4 +1,8 @@
 package com.example.miniamazon.Repository;
 
-public interface SellerRepository {
+import com.example.miniamazon.Model.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+    /**/
 }
