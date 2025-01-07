6871eaa502c85ee51bd574a5385e6a93a3522424
MohamedYehia20m <medoyehia2001@gmail.com>
2025-01-07 20:58:28 +0200

    remove unused Architecture Component (Seller)

--- src/main/java/com/example/miniamazon/Repository/SellerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.miniamazon.Repository;
-
-import com.example.miniamazon.Model.Seller;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SellerRepository extends JpaRepository<Seller, Long> {
-    /**/
-}
