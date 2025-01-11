2aa5c3efe2374131faaedcef4a20627549ceb9f6
Mohd-Sherif <Mohd-Sherif@outlook.com>
2025-01-11 22:21:17 +0200

    Create new entity called `Order` to join `Product` with `User`

---
+++ src/main/java/com/example/miniamazon/Model/Order.java
@@ -0,0 +1,36 @@
+package com.example.miniamazon.Model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String status;
+    private Double totalAmount;
+
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_products",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private Set<Product> products;
+}
